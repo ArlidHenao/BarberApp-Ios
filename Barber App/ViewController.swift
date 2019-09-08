e75c308f08ea35704de578c7904c17a6d5843d0b
Arlid Henao Rueda <arliid@hotmail.com>
2019-09-08 18:39:54 -0500

    Initial Commit

---
+++ Barber App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Barber App
+//
+//  Created by Arlid Henao Rueda on 9/8/19.
+//  Copyright © 2019 Arlid Henao Rueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
